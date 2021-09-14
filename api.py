9dde4b880aac0ed0143c32968d54dee435a8a2bd
Rishiraj Acharya <44090649+rishiraj@users.noreply.github.com>
2021-09-14 20:30:44 +0530

    Update api.py

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def upload_predict():
-  return render_template("index.html")
+  return render_template("index.html", prediction=0)
 
 if __name__ == "__main__":
   app.run(port=12000, debug=True)
